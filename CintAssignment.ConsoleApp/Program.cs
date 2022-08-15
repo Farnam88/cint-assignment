3ab9c73abfa78fb0451287bde59eed18e25e4b2b
Farnam <masoud.jamshidian88@gmail.com>
2022-08-15 21:39:38 +0200

    Receive inputs
    receive inputs and store them in variables based on the instruction, input validation is not required(explained in instruction doc)

--- CintAssignment.ConsoleApp/Program.cs
+++ CintAssignment.ConsoleApp/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Please Enter number of commands:");
+
+Int32.TryParse(Console.ReadLine(), out int commandQuantity);
+
+Console.WriteLine("Please Enter starting point:");
+
+string startingPoint = Console.ReadLine()!;
+
+Console.WriteLine("Please Enter Commands:");
+
+var command = new StringBuilder();
+for (int i = 0; i < commandQuantity; i++)
+{
+    command.Append(Console.ReadLine()!);
+    command.Append(" ");
+}
